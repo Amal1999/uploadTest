e29af56314af53e8406cd3d45a4a9064e48a23a2
Sammari Amal <58918108+Amal1999@users.noreply.github.com>
2022-06-29 22:41:39 +0100

    first commit

---
+++ lib/ui/widgets/waiting.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/Material.dart';
+
+class Waiting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+      ),
+    );
+  }
+}
